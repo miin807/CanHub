6718e65e1cceeaf6a884619ad925a92663fb3391
anahi hinojosa <ana11hinograje@gmail.com>
2025-01-15 13:55:58 +0100

    terminadas Ajustes y Perfil, con traduccion a ingles hecha

--- app/src/main/java/com/canhub/canhub/Ajustes.java
+++ app/src/main/java/com/canhub/canhub/Ajustes.java
@@ -10,31 +10,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignUp extends AppCompatActivity {
+public class Ajustes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
-
-
-
+        setContentView(R.layout.activity_ajustes);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 
-    public void returnLogin (View view){
-        Intent intent = new Intent(SignUp.this,Login.class);
-        startActivity(intent);
-    }
-
-    public void goAjustes(View view) {
-        Intent intent = new Intent(SignUp.this,Ajustes.class);
+    public void goPerfil(View view) {
+        Intent intent = new Intent(Ajustes.this,Perfil.class);
         startActivity(intent);
     }
 }
