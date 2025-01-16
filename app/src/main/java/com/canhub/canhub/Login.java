1a4cc1d48699c3be01e6830233ad03b16f1a1aa3
Min Wang <minwang807@gmail.com>
2025-01-16 10:16:39 +0100

    Añadido cambios en activity busqueda

--- app/src/main/java/com/canhub/canhub/Login.java
+++ app/src/main/java/com/canhub/canhub/Login.java
@@ -26,14 +26,8 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    public void goToSignup (View view){
-        Intent intent = new Intent(Login.this,SignUp.class);
-        startActivity(intent);
+//    public void goToSignup (View view){
+//        Intent intent = new Intent(Login.this,SignUp.class);
+//        startActivity(intent);
 
     }
-
-    public void goMain (View view){
-        Intent intent = new Intent(Login.this,MainActivity.class);
-        startActivity(intent);
-    }
-}
