9439fa2b418fe5faa35175e3fa045d2561d4fcea
Marina <marina.sierra3@educa.madrid.org>
2025-01-20 13:39:06 +0100

    --

--- app/src/main/java/com/canhub/canhub/SignUp.java
+++ app/src/main/java/com/canhub/canhub/SignUp.java
@@ -3,6 +3,7 @@ package com.canhub.canhub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +19,15 @@ public class SignUp extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
 
+        Button b = findViewById(R.id.registrarse);
 
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent4=new Intent(SignUp.this, Inicio.class);
+                startActivity(intent4);
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -31,11 +40,6 @@ public class SignUp extends AppCompatActivity {
     public void returnLogin (View view){
         Intent intent3 = new Intent(SignUp.this,Login.class);
         startActivity(intent3);
-    }
-
-    public void inicio(View view){
-        Intent intent4=new Intent(this, Inicio.class);
-        startActivity(intent4);
     }
 
 }
