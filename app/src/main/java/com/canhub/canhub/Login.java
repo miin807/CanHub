d4707fbe6a657635260f5a4617040fd254aaa6e9
Marina <marina.sierra3@educa.madrid.org>
2025-03-05 13:25:28 +0100

    --

--- app/src/main/java/com/canhub/canhub/Login.java
+++ app/src/main/java/com/canhub/canhub/Login.java
@@ -1,5 +1,7 @@
 package com.canhub.canhub;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
